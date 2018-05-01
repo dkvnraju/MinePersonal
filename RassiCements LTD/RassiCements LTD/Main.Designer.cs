b0cb5d9812f99e225d054e2d3d780ece14914578
DKVNRAJU <DKVNRAJU@DESKTOP-5VIV1M0>
2018-05-01 16:19:36 +0530

    label font change

--- RassiCements LTD/RassiCements LTD/Main.Designer.cs
+++ RassiCements LTD/RassiCements LTD/Main.Designer.cs
@@ -1905,9 +1905,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(227, 47);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(166, 32);
+            this.label3.Size = new System.Drawing.Size(244, 46);
             this.label3.TabIndex = 1;
             this.label3.Text = "Other Batch";
             // 
@@ -1923,9 +1924,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(252, 113);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(225, 32);
+            this.label5.Size = new System.Drawing.Size(334, 46);
             this.label5.TabIndex = 3;
             this.label5.Text = "Other Contractor";
             // 
