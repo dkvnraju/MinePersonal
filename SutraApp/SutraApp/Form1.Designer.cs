a8b7cd9f33551bf57e9434ef18a397a814b42957
DKVNRAJU <DKVNRAJU@DESKTOP-5VIV1M0>
2018-06-02 22:20:33 +0530

    Adding Home Panel

--- SutraApp/SutraApp/Form1.Designer.cs
+++ SutraApp/SutraApp/Form1.Designer.cs
@@ -28,13 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.HomePanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // HomePanel
+            // 
+            this.HomePanel.BackColor = System.Drawing.SystemColors.Control;
+            this.HomePanel.Location = new System.Drawing.Point(38, 26);
+            this.HomePanel.Name = "HomePanel";
+            this.HomePanel.Size = new System.Drawing.Size(3650, 2048);
+            this.HomePanel.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(3719, 2108);
+            this.Controls.Add(this.HomePanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +52,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel HomePanel;
     }
 }
 
